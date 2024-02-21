f30c8103af3484f3b20594c9c1874ece3b067148
RahulGupta <rahulrgd21@gmail.com>
2024-02-22 03:03:22 +0530

    Step 09 - Implementing POST Method to create User Resource

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -3,25 +3,38 @@ package com.in28minutes.rest.webservices.restfulwebservices.user;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserDaoService {
-    
-    private static List<User> users = new ArrayList<>();
 
-    static{
-        users.add(new User(1, "Rahul", LocalDate.now().minusYears(25)));
-        users.add(new User(2, "Rahi", LocalDate.now().minusYears(25)));
-        users.add(new User(3, "Satendra", LocalDate.now().minusYears(25)));
-    }
+  private static List<User> users = new ArrayList<>();
 
-    public List<User> findAll(){
-        return users;
-    }
+  public static Integer userCount = 0;
 
-    public User findUser(Integer id){
-        return users.stream().filter(user -> user.getId().equals(id)).findFirst().get();
-    }
+  static {
+    users.add(new User(++userCount, "Rahul", LocalDate.now().minusYears(25)));
+    users.add(new User(++userCount, "Rahi", LocalDate.now().minusYears(25)));
+    users.add(
+      new User(++userCount, "Satendra", LocalDate.now().minusYears(25))
+    );
+  }
+
+  public List<User> findAll() {
+    return users;
+  }
+
+  public User findUser(Integer id) {
+    return users
+      .stream()
+      .filter(user -> user.getId().equals(id))
+      .findFirst()
+      .get();
+  }
+
+  public User save(User user) {
+    user.setId(++userCount);
+    users.add(user);
+    return user;
+  }
 }
