81ef409ff55b98fea8e689f434ceb27ccf453349
RahulGupta <rahulrgd21@gmail.com>
2024-02-22 02:40:57 +0530

    Step 08 - Implementing GET Methods for User Resource

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -9,15 +9,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserDaoService {
     
-    private List<User> users = new ArrayList<>();
+    private static List<User> users = new ArrayList<>();
 
     static{
-        new User(1, "Rahul", LocalDate.now().minusYears(25));
-        new User(2, "Rahi", LocalDate.now().minusYears(25));
-        new User(3, "Satendra", LocalDate.now().minusYears(25));
+        users.add(new User(1, "Rahul", LocalDate.now().minusYears(25)));
+        users.add(new User(2, "Rahi", LocalDate.now().minusYears(25)));
+        users.add(new User(3, "Satendra", LocalDate.now().minusYears(25)));
     }
 
     public List<User> findAll(){
         return users;
+    }
+
+    public User findUser(Integer id){
+        return users.stream().filter(user -> user.getId().equals(id)).findFirst().get();
     }
 }
