7a24dc0cf60854ab9b2c2128732a341beb58c507
RahulGupta <rahulrgd21@gmail.com>
2024-02-22 22:17:28 +0530

    Step 23 - Implementing Static Filtering for REST API

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -4,14 +4,18 @@ import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class User {
 
   private Integer id;
 
   @Size(min = 2, message = "Name must contain atleast 2 characters")
+  @JsonProperty("user_name")
   private String name;
 
   @Past(message = "BirthDate must be in pats")
+  @JsonProperty("birth_date")
   private LocalDate birthDate;
 
   public User(Integer id, String name, LocalDate birthDate) {
